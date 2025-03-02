47707ee1cc6759ec9a9fddc2a01b9c26320fdc4d
DevRamona <r.ingabire@alustudent.com>
2025-03-02 21:12:54 +0200

    change console to alert

--- Javascript_advanced/7-timeout.js
+++ Javascript_advanced/7-timeout.js
@@ -1,11 +1,11 @@
 console.log("Start of the execution queue");
 
 setTimeout(() => {
-  console.log("Final code block to be executed");
+  alert("Final code block to be executed");
 }, 0);
 
 for (let i = 1; i <= 100; i++) {
   console.log(i);
 }
 
-console.log("End of the loop printing");
+console.log("End of the loop printing by");
